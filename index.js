391949808b5e46432543e25323bdebbddfc1b59e
Gabriela Pompeo <gabrielaoliveirapompeo1234@gmail.com>
2026-04-06 20:52:18 -0300

    Ajuste dropdown menu

--- index.js
+++ index.js
@@ -69,29 +69,40 @@ app.get('/menu',veri,(req,res)=>{
         <body>`);
 
    res.write(`
+
 <nav class="navbar navbar-expand-lg bg-body-tertiary shadow-sm">
+
   <div class="container-fluid">
     <a class="navbar-brand fw-semibold" href="/menu">Biblioteca</a>
+    
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
+
     <div class="collapse navbar-collapse" id="navbarNav">
+
       <ul class="navbar-nav ms-auto">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="/menu">Home</a>
         </li>
+
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Cadastro
           </a>
-          <ul class="dropdown-menu">
+
+          <ul class="dropdown-menu dropdown-menu-end">
             <li><a class="dropdown-item" href="/livro">Cadastro de Livros</a></li>
+
             <li><a class="dropdown-item" href="/listaLivros">Listar Livros</a></li>
+
             <li><hr class="dropdown-divider"></li>
             <li><a class="dropdown-item" href="/leitor">Cadastro de Leitores</a></li>
+
             <li><a class="dropdown-item" href="/listaLeitores">Listar Leitores</a></li>
           </ul>
         </li>
+
         <li class="nav-item">
           <a class="nav-link" href="/logout">Sair</a>
         </li>
